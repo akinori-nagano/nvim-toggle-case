2fb5d665d851d74f7dbaf533cda80635f27c1e25
nagano <nagano@kickmogu.com>
2020-04-01 13:44:21 +0900

    first commit.

---
+++ rplugin/python3/nvim-toggle-case/__init__.py
@@ -0,0 +1,14 @@
+import neovim
+
+@neovim.plugin
+class NvimCMakeProj(object):
+
+    def __init__(self, nvim):
+        self.__nvim = nvim
+
+    def echo(self, msg):
+        self.nvim.command("echo '" + msg + "'")
+
+    @neovim.command('CMakeReload')
+    def cmake_reload(self):
+        self.echo("test")'")
